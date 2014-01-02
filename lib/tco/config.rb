5f0cfd11105d5752d312d8802487fbb6cc97d9da
Radek Pazdera <radek@pazdera.co.uk>
2014-01-02 13:21:10 +0000

    lib: Finishing Config implementation
    The Config class was only a stub. This commit adds all the parsing
    routines and all other required functionality.

--- lib/tco/config.rb
+++ lib/tco/config.rb
@@ -1,57 +1,122 @@
-require "yaml"
+# tco - terminal colouring application and library
+# Copyright (C) 2013 Radek Pazdera
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'yaml'
 
 module Tco
-  DEFAULT_CONFIG_PATH = "~/.tco-conf"
-  @configuration = {
-    :options => {
-      :palette => "auto", # auto, ansi, extended
+  class Configuration
+    attr_reader :palette, :colour_values, :names, :styles
 
-      :ansi_colours => {
-        "@0" => 0x000000,
-        "@1" => 0xCC0000,
-        "@2" => 0x4E9A06,
-        "@3" => 0xC4A000,
-        "@4" => 0x3465A4,
-        "@5" => 0x75507B,
-        "@6" => 0x06989A,
-        "@7" => 0xD3D7CF,
-        "@8" => 0x555753,
-        "@9" => 0xEF2929,
-        "@10" => 0x8AE234,
-        "@11" => 0xFCE94F,
-        "@12" => 0x729FCF,
-        "@13" => 0xAD7FA8,
-        "@14" => 0x34E2E2,
-        "@15" => 0xEEEEEC
-      },
-    },
+    def initialize
+      @locations = ["/etc/tco.conf", "~/.tco.conf"]
 
-    :aliases => {
-      "black" => "@0",
-      "red" => "@1",
-      "green" => "@2",
-      "yellow" => "@3",
-      "blue" => "@4",
-      "magenta" => "@5",
-      "cyan" => "@6",
-      "light-gray" => "@7",
-      "dark-gray" => "@8",
-      "light-red" => "@9",
-      "light-green" => "@10",
-      "light-yellow" => "@11",
-      "light-blue" => "@12",
-      "light-magenta" => "@13",
-      "light-cyan" => "@14",
-      "white" => "@15"
-    },
+      @palette = "auto"
+      @colour_values = {}
+      @names = {
+        "black" => "@0",
+        "red" => "@1",
+        "green" => "@2",
+        "yellow" => "@3",
+        "blue" => "@4",
+        "magenta" => "@5",
+        "cyan" => "@6",
+        "light-gray" => "@7",
+        "dark-gray" => "@8",
+        "light-red" => "@9",
+        "light-green" => "@10",
+        "light-yellow" => "@11",
+        "light-blue" => "@12",
+        "light-magenta" => "@13",
+        "light-cyan" => "@14",
+        "white" => "@15"
+      }
 
-    :presets => {
-      "alert" => ["red", "default", true, false]
-    }
-  }
+      @styles = {}
 
-  def self.load_config(path=DEFAULT_CONFIG_PATH)
-    puts @configuration
-    return unless File.exists? path
+      @locations.each do |conf_file|
+        next unless File.exists? conf_file
+        load_config_file conf_file
+      end
+    end
+
+    private
+    def name_exists?(colour_name)
+      @names.has_key? colour_name
+    end
+
+    def parse_bool(value)
+     return true if value =~ /true/i || value =~ /yes/i || value.to_i >= 1
+     return false
+    end
+
+    def load_config_file(path)
+      conf_file = YAML::load_file path
+
+      if conf_file.has_key? "palette"
+          @palette = conf_file["palette"]
+      end
+
+      if conf_file.has_key? "colour_values"
+        if conf_file["colour_values"].is_a? Hash
+          conf_file["colour_values"].each do |id, value|
+            @colour_values[id] = value
+          end
+        else
+          raise "The 'colour_values' config option must be a hash."
+        end
+      end
+
+      if conf_file.has_key? "names"
+        if conf_file["names"].is_a? Hash
+          conf_file["names"].each do |name, colour|
+            @names[name] = colour
+          end
+        else
+          raise "Invalid format of the 'names' option."
+        end
+      end
+
+      if conf_file.has_key? "styles"
+        if conf_file["styles"].is_a? Hash
+          conf_file["styles"].each do |name, styledef|
+            style = {:fg => nil, :bg => nil, :bright => false,
+                     :underline => false}
+
+            if styledef.has_key? "fg"
+              style[:fg] = styledef["fg"]
+            end
+
+            if styledef.has_key? "bg"
+              style[:bg] = styledef["bg"]
+            end
+
+            if styledef.has_key? "bright"
+              style[:bright] = parse_bool styledef["bright"]
+            end
+
+            if styledef.has_key? "underline"
+              style[:underline] = parse_bool styledef["underline"]
+            end
+
+            @styles[name] = style
+          end
+        else
+          raise "Invalid format of the 'styles' option."
+        end
+      end
+    end
   end
 end
