6260eee885a5db225dd8acbffe545d27521deb57
Radek Pazdera <radek@pazdera.co.uk>
2014-01-05 19:49:00 +0000

    spec: Starting over
    Removed the irrelevant test and added one for the HSL colour conversion.
    
    The coverage at this point is just sad though.

--- spec/tco_spec.rb
+++ spec/tco_spec.rb
@@ -1,7 +1,16 @@
 require "tco"
 
 describe Tco do
-  it "should load config" do
-    config = Tco.load_config
+  describe Tco::Colour do
+    before :each do
+      @colour = Tco::Colour.new [12, 34, 56]
+    end
+
+    it "converts to HSL" do
+      hsl = @colour.hsl
+      hsl[0].should be_within(0.1).of 210
+      hsl[1].should be_within(0.1).of 64.7
+      hsl[2].should be_within(0.1).of 13.3
+    end
   end
 end
