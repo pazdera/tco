f49ea73329885f4c8e99a503c7aa6a838d6b0d6f
Radek Pazdera <radek@pazdera.co.uk>
2014-03-29 12:23:56 +0000

    examples: Improving the rainbow example

--- examples/rainbow.rb
+++ examples/rainbow.rb
@@ -2,7 +2,17 @@
 
 require "tco"
 
-rainbow = ["#622e90", "#2d3091", "#00aaea", "#02a552", "#fdea22", "#eb443b", "#f37f5a"]
+conf = Tco.config
+conf.names["purple"] = "#622e90"
+conf.names["dark-blue"] = "#2d3091"
+conf.names["blue"] = "#00aaea"
+conf.names["green"] = "#02a552"
+conf.names["yellow"] = "#fdea22"
+conf.names["orange"] = "#f37f5a"
+conf.names["red"] = "#eb443b"
+Tco.reconfigure conf
+
+rainbow = ["purple", "dark-blue", "blue", "green", "yellow", "orange", "red"]
 10.times do
   rainbow.each { |colour| print "    ".bg colour }
   puts
